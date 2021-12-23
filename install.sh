e64ac1b938fd066d11adf4c655531d4a43b0d9db
Yuzukiko <rafal.p@outlook.com>
2021-12-23 16:49:58 +0100

    .

--- install.sh
+++ install.sh
@@ -16,15 +16,18 @@ sudo usermod -a -G docker pi
 #docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-aarch64" -o /usr/local/bin/docker-compose
 
-#kubernetes (microk8s)
-sudo apt install snapd
-sudo snap install microk8s --classic
-sudo usermod -a -G microk8s pi
-echo "alias kubectl='microk8s.kubectl'" >> ~/.bashrc
-cd $HOME
-mkdir .kube
-cd .kube
-microk8s config > config
+#kubernetes (microk8s) 
+#sudo apt install snapd
+#sudo snap install microk8s --classic
+#sudo usermod -a -G microk8s pi
+#echo "alias kubectl='microk8s.kubectl'" >> ~/.bashrc
+#cd $HOME
+#mkdir .kube
+#cd .kube
+#microk8s config > config
+
+#kubernetes (k3s)
+sudo curl -sfL "https://get.k3s.io" | sh -
 
 #reboot
 sudo reboot
